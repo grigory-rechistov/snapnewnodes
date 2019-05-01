e38e6021821a399cd72c07459f278654335d0088
Grigory Rechistov <grigory.rechistov@phystech.edu>
2019-05-01 15:53:14 +0200

    Add proper license headers

--- src/org/openstreetmap/josm/plugins/snapnewnodes/SnapNewNodesPreferenceSetting.java
+++ src/org/openstreetmap/josm/plugins/snapnewnodes/SnapNewNodesPreferenceSetting.java
@@ -1,4 +1,20 @@
-// License: GPL. For details, see LICENSE file.
+/*
+ * This file is part of SnapNewNodes plugin
+ * Copyright (c) 2019 Grigory Rechistov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 or later
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.openstreetmap.josm.plugins.snapnewnodes;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
